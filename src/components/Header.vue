66f1e1bf3cba479d7d36f34839bfbeb5e16f29fd
bsv-trinh-tran <trinhtran@bravesoft.com.vn>
2023-08-26 13:41:48 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="header">
+        <div class="container">
+            <div class="logo">
+                <router-link to="">
+                    <h1>
+                        L
+                        <img src="/images/heart-solid.svg" alt="" />
+                        U
+                    </h1>
+                </router-link>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li>
+                        <router-link to="">Cặp đôi</router-link>
+                    </li>
+                    <li>
+                        <router-link to="">Sự kiện</router-link>
+                    </li>
+                    <li>
+                        <router-link to="">Chuyện tình yêu</router-link>
+                    </li>
+                    <li>
+                        <router-link to="">Album cưới</router-link>
+                    </li>
+                    <li>
+                        <router-link to="">Phù dâu và phù rể</router-link>
+                    </li>
+                    <li>
+                        <router-link to="">lời chúc</router-link>
+                    </li>
+                    <li>
+                        <router-link to="">mừng cưới</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.header {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    min-height: 50px;
+    position: absolute;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    text-transform: uppercase;
+    font-weight: bold;
+    .container {
+        width: 1170px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-top: 10px;
+        .logo {
+            font-family: "Playball", cursive;
+            h1 {
+                display: flex;
+                font-size: 50px;
+                img {
+                    width: 50px;
+                    height: 50px;
+                    color: red;
+                    margin: 0px 10px;
+                }
+            }
+        }
+        .menu {
+            ul {
+                display: flex;
+                list-style-type: none;
+                gap: 30px;
+                font-size: 14px;
+                font-weight: bold;
+                font-family: "Quicksand", sans-serif;
+                li {
+                    &:hover {
+                        transform: translateY(-5px);
+                    }
+                }
+            }
+        }
+        a {
+            text-decoration: none;
+            color: azure;
+        }
+    }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="main">
+        <Header></Header>
+        <div class="swiper-container">
+            <swiper
+                :modules="modules"
+                :centeredSlides="true"
+                :navigation="true"
+                :autoplay="{
+                    delay: 20000000,
+                    disableOnInteraction: true,
+                }"
+                class="mySwiper"
+            >
+                <swiper-slide>
+                    <div class="background"></div>
+                    <!-- <img src="/images/swiper-3.jpg" alt="" /> -->
+                </swiper-slide>
+                <swiper-slide>
+                    <div class="background"></div>
+                    <!-- <img src="/images/swiper-2.jpg" alt="" /> -->
+                </swiper-slide>
+            </swiper>
+        </div>
+        <div class="display-tc">
+            <h1>Save the date</h1>
+            <div class="banner-text">
+                <span class="banner-text_names">Ngọc Linh </span>
+                <img src="/images/heart-solid.svg" alt="" />
+                <span class="banner-text_names">Ngọc Uyên </span>
+            </div>
+            <div class="banner-text_date">16 October 2023</div>
+        </div>
+    </div>
+</template>
+<script setup>
+import Header from "../components/Header.vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+const modules = [Autoplay, Pagination, Navigation];
+</script>
+<style lang="scss">
+.main {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+}
+.swiper-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+}
+.background {
+    background: red;
+}
+.swiper-button-next,
+.swiper-button-prev {
+    background: rgba(248, 255, 254, 0.81);
+    width: 55px;
+    height: 55px;
+    line-height: 50px;
+    margin-top: -30px;
+    text-align: center;
+    border-radius: 60px;
+    border: 4px solid rgba(248, 255, 254, 0.91);
+    z-index: 10;
+    opacity: 1;
+    right: 10px;
+    left: auto;
+    &::after {
+        font-size: 16px;
+        color: #3e7884;
+    }
+    &:hover {
+        opacity: 0.5;
+    }
+}
+.swiper-button-prev {
+    left: 10px;
+    right: auto;
+}
+.display-tc {
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    h1 {
+        font-size: 150px;
+        font-family: "Amatic SC", cursive;
+        font-weight: 700;
+        color: white;
+    }
+    .banner-text {
+        color: #ffffff;
+        display: flex;
+        justify-content: center;
+        .banner-text_names {
+            font-family: "Playball", cursive;
+            font-size: 45px;
+            line-height: 48px;
+        }
+        img {
+            width: 30px;
+            height: 30px;
+            margin: 0px 15px;
+        }
+    }
+    .banner-text_date {
+        display: flex;
+        font-size: 25px;
+        line-height: 28px;
+        color: #fff;
+        margin-top: 10px;
+        font-family: "Quicksand", sans-serif;
+    }
+}
+</style>
